647c66b7ac8ff2380bc5fd493297bf6d837b5c00
DouglasRibeiro1972 <douglascri@yahoo.com.br>
2022-09-04 15:01:17 -0300

    criando novo teste

---
+++ pasta3/outroteste.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int num;
+    printf("digite um numero: ");
+    scanf("%d", &num);
+    
+    printf("%d", num);
+    printf("hello, World");
+
+
+return 0;
+}
