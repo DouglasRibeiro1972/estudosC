39dffde378549ce31f1415eed5c69f72baa2cb08
DouglasRibeiro1972 <douglascri@yahoo.com.br>
2022-09-12 21:00:51 -0300

    criei_teste_c#

---
+++ teste2_git.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double nota1, nota2, nota3, nota4, media;
+        
+        Console.WriteLine("nota1");
+        nota1 = readValue();
+        Console.WriteLine("nota2");
+        nota2 = readValue();
+        Console.WriteLine("nota3");
+        nota3 = readValue();
+        Console.WriteLine("nota4");
+        nota4 = readValue();
+        media = (nota1 + nota2 + nota3 + nota4) / 4;
+        Console.WriteLine("a media é: " + media);
+        if (media >= 7)
+        {
+            Console.WriteLine("aprovado");
+        }
+        else
+        {
+            Console.WriteLine("reprovado");
+        }
+    }
+    
+    // .NET can only read single characters or entire lines from the console.
+    // The following function safely reads a double value.
+    private static double readValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result));
+        return result;
+    }
+}
